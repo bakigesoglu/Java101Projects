9141f32d79d3d11a99cd374c88e2ed1e3e2918c2
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-01-26 00:29:28 +0300

    Projects

---
+++ Triangle.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/*
+@author bakigesoglu
+ */
+class Hypotenuse {
+    public static void main(String[] args) {
+        // Hypothenuse of Angle's inputed legs length from keyboard
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the angle's first leg length: ");
+        Double firstLeg = input.nextDouble();
+        System.out.print("Enter the angle's second leg length: ");
+        Double secondLeg = input.nextDouble();
+        Double hypo= (firstLeg*firstLeg) + (secondLeg*secondLeg);
+        Double result = Math.sqrt(hypo);
+
+        System.out.print("Angle's hypotenuse length: "+ result);
+
+
+    }
+}
+class Square {
+    public static void main(String[] args){
+        Scanner inp= new Scanner(System.in);
+        System.out.print("Enter The angle's first edge length: ");
+        Double firstEdge = inp.nextDouble();
+        System.out.print("Enter the angle's second edge length: ");
+        Double secondEdge = inp.nextDouble();
+        System.out.print("Enter the angle's third edge length: ");
+        Double thirdEdge = inp.nextDouble();
+        Double roundofAngle= (firstEdge+secondEdge+thirdEdge);
+        Double u= roundofAngle/2;
+        Double angleSquare1= u*(u-firstEdge)*(u-secondEdge)*(u-thirdEdge);
+        Double angleSquare= Math.sqrt(angleSquare1);
+        System.out.print("The Triangle's Area: "+ angleSquare);
+    }
+}
+
